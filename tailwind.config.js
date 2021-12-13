36fbefb40b363d2075e5fd9d164984494ca7bf24
Chase Isley <isleychase@gmail.com>
2021-12-13 10:48:44 -0800

    update all deps and tailwind 3 config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: {
-    enabled: true,
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  },
-  darkMode: false,
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       transitionDuration: {
