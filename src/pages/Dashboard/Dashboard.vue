d45ce42f04043e27f9e8cd4b2156458f4b0dd810
Chase Isley <isleychase@gmail.com>
2021-12-17 03:12:01 -0800

    update dash with repo link

--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -7,5 +7,14 @@
     <li>Reactive, auto generated controls from component props</li>
     <li>Built with Vue3, Vite, Vue Router 4 and Vuex</li>
     <li>Styled with tailwind</li>
+    <li>
+      <a
+        class="text-blue-500 hover:text-blue-600"
+        href="https://github.com/theonlychase/ui-library"
+        target="_blank"
+      >
+        UI Library Repo
+      </a>
+    </li>
   </ul>
 </template>
